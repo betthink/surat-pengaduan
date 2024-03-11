72e356ffa027d5827ac847d8207932d4dcf92205
betthink <robetszone@gmail.com>
2024-03-11 18:58:17 +0700

    feat : add navbar and sub header mobile

--- resources/views/public/layouts/components/header.blade.php
+++ resources/views/public/layouts/components/header.blade.php
@@ -5,7 +5,6 @@
                 <div class="header__logo ">
                     <a class="text-light d-flex justify-content-between align-items-center w-75"
                         href="{{ route('Beranda') }}">
-                        {{-- <img src="{{ asset('cool-admin/images/icon/logo.png ') }}" alt="Cool Admin" /> --}}
                         <i class="fa fa-book fa-2xl" aria-hidden="true"></i>
                         <h3 class="text-light">Pengaduan</h3>
 
@@ -27,65 +26,10 @@
                         </li>
                         <li>
                             <a href="{{ route('public-hasil') }}">
-                         <i class="fas fa-balance-scale"></i>
+                                <i class="fas fa-balance-scale"></i>
                                 <span class="bot-line"></span>Hasil</a>
                         </li>
-                        {{-- <li class="has-sub">
-                            <a href="#">
-                                <i class="fas fa-copy"></i>
-                                <span class="bot-line"></span>Pages</a>
-                            <ul class="header3-sub-list list-unstyled">
-                                <li>
-                                    <a href="login.html">Login</a>
-                                </li>
-                                <li>
-                                    <a href="register.html">Register</a>
-                                </li>
-                                <li>
-                                    <a href="forget-pass.html">Forget Password</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="has-sub">
-                            <a href="#">
-                                <i class="fas fa-desktop"></i>
-                                <span class="bot-line"></span>UI Elements</a>
-                            <ul class="header3-sub-list list-unstyled">
-                                <li>
-                                    <a href="button.html">Button</a>
-                                </li>
-                                <li>
-                                    <a href="badge.html">Badges</a>
-                                </li>
-                                <li>
-                                    <a href="tab.html">Tabs</a>
-                                </li>
-                                <li>
-                                    <a href="card.html">Cards</a>
-                                </li>
-                                <li>
-                                    <a href="alert.html">Alerts</a>
-                                </li>
-                                <li>
-                                    <a href="progress-bar.html">Progress Bars</a>
-                                </li>
-                                <li>
-                                    <a href="modal.html">Modals</a>
-                                </li>
-                                <li>
-                                    <a href="switch.html">Switchs</a>
-                                </li>
-                                <li>
-                                    <a href="grid.html">Grids</a>
-                                </li>
-                                <li>
-                                    <a href="fontawesome.html">FontAwesome</a>
-                                </li>
-                                <li>
-                                    <a href="typo.html">Typography</a>
-                                </li>
-                            </ul>
-                        </li> --}}
+
                     </ul>
                 </div>
                 <div class="header__tool">
@@ -132,6 +76,12 @@
         <div class="header-mobile__bar">
             <div class="container-fluid">
                 <div class="header-mobile-inner">
+                    <a class="text-light d-flex justify-content-between align-items-center w-75"
+                        href="{{ route('Beranda') }}">
+                        <i class="fa fa-book fa-2xl" aria-hidden="true"></i>
+                        <h3 class="text-light">Pengaduan</h3>
+
+                    </a>
                     <button class="hamburger hamburger--slider" type="button">
                         <span class="hamburger-box">
                             <span class="hamburger-inner"></span>
@@ -140,5 +90,60 @@
                 </div>
             </div>
         </div>
+        <nav class="navbar-mobile">
+            <div class="container-fluid">
+                <ul class="navbar-mobile__list list-unstyled">
+                    <li class="has-sub">
+                        <a href="{{ route('Beranda') }}">
+                            <i class="fas fa-home"></i>Beranda
+                            <span class="bot-line"></span>
+                        </a>
 
+                    </li>
+                    <li>
+                        <a href="{{ route('public-pengaduan') }}">
+                            <i class="far fa-envelope-open"></i>
+                            <span class="bot-line"></span>Pengaduan</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('public-hasil') }}">
+                            <i class="fas fa-balance-scale"></i>
+                            <span class="bot-line"></span>Hasil</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </header>
+    <div class="sub-header-mobile-2 d-block d-lg-none">
+        <div class="header__tool">
+            <div class="account-wrap">
+                <div class="account-item account-item--style2 clearfix js-item-menu">
+                    <div class="content">
+                        <a class="js-acc-btn" href="#"> @isset($user)
+                                {{ $user['username'] }}
+                            @endisset
+                        </a>
+                    </div>
+                    <div class="account-dropdown js-dropdown">
+                        <div class="info clearfix">
+                            <div class="content">
+                                <h5 class="name">
+                                    @isset($user)
+                                        {{ $user['username'] }}
+                                    @endisset
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="account-dropdown__footer">
+                            <a href="#" data-bs-toggle="modal" data-bs-target="#profileModal">
+                                <i class="far fa-user"></i>Profile</a>
+                        </div>
+                        <div class="account-dropdown__footer">
+                            <a href="{{ route('public-logout') }}">
+                                <i class="zmdi zmdi-power"></i>Logout</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
